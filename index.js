03efb7a68196fc804c1a14c69caba92ec20f0682
SvyatoslavGabuda <sgabuda@gmail.com>
2023-02-22 14:40:42 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var newName = "Pietro";
+var aNumber = 2;
+var aNumOrString = "2";
+var a = 2;
+a = "a";
+var arrOfString = ["a", "b"];
+var arrOfString2 = ["a", "b"];
---
+++ index.ts
@@ -0,0 +1,12 @@
+let newName:string = "Pietro";
+
+let aNumber:number = 2;
+let aNumOrString: number|string= "2"
+
+type numOrStr = number |string
+
+let a:numOrStr=2
+a="a"
+
+let arrOfString:string[] = ["a","b"]
+let arrOfString2:Array<string>=["a","b"]
